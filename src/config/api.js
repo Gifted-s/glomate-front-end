13e4b841a599c941f08a801c97b08a079b3afe4c
Gifted-s <sunkanmiadewumi1@gmail.com>
2022-02-01 10:21:21 +0100

    added mpre features

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 const apiConfig = {
-    root:"http://localhost:8080"
-    //root:"https://glomate.herokuapp.com"
+    //root:"http://localhost:8080"
+    root:"https://glomate.herokuapp.com"
 }
 
 export default apiConfig
